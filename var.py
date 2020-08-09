5e7267a0daff9a508ae676962199e58d79674efc
unicornrelish <69436168+unicornrelish@users.noreply.github.com>
2020-08-09 16:55:15 -0400

    Create var.py

---
+++ var.py
@@ -0,0 +1,15 @@
+#Variables imported to Doge_Bot
+TOKEN = open('token.txt','r').read()
+
+ver = '0.0.5'
+owner = 'unicornrelish#7310'
+bot = 'Doge_Bot#5885'
+guild = 'Dogenet Incorporated'
+mod_channel_id = 740025696113655878
+bot_cmd_channel = 'bot_commands'
+
+
+steam_group_url = None
+mcserver_ip = 'dogenetmc.duckdns.org'
+
+b_words = ['bad words go here']
