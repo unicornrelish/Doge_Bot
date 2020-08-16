bf7ffc32cab2ca151a594e307475adc6f52abca7
unicornrelish <69436168+unicornrelish@users.noreply.github.com>
2020-08-16 12:23:39 -0400

    Update bot.py
    Main for 0.1.0

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
-#Doge_Bot - Ver 0.0.2
+#Doge_Bot - Ver 0.1.0
 import discord
 from discord.ext import commands,tasks
 import time
 import asyncio
 from itertools import cycle
 import os
-import word_filter
-from var import steam_group_url, mcserver_ip, bot_cmd_channel, ver, owner, mod_channel_id, guild, TOKEN, b_words, bot
+from var import steam_group_url, mcserver_ip, bot_cmd_channel, ver, owner, mod_channel_id, guild, TOKEN, b_words, bot, mod_role
 
 
 ready_act = discord.Game(f'on version {ver}')
 processing_act = discord.Game('with the code.')
+
+
 
 messages = joined = 0
 
@@ -18,22 +19,24 @@ cmd_prefix='!'
 
 GUILD = guild
 
-client = discord.Client()
+description = 'The Dogenet Incorporated Master Bot'
+bot = commands.Bot(command_prefix=cmd_prefix, description=description)
+
 
 #Sets the load message when not idle.
 async def load():
-    await client.change_presence(activity=processing_act, status=discord.Status.online)
+    await bot.change_presence(activity=processing_act, status=discord.Status.online)
 
 #Sets the idle message when not under load.
 async def idle():
-    await client.change_presence(status=discord.Status.idle, activity=ready_act)
+    await bot.change_presence(status=discord.Status.idle, activity=ready_act)
 
 #Updates the stats file and log file.
 async def update_stats():
-    await client.wait_until_ready()
+    await bot.wait_until_ready()
     global messages, joined
 
-    while not client.is_closed():
+    while not bot.is_closed():
         try:
             with open('stats.txt', 'a') as f:
                 f.write(f'Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n')
@@ -46,32 +49,28 @@ async def update_stats():
             await asyncio.sleep(1200)
 
 async def update_logs():
-    await client.wait_until_ready()
+    await bot.wait_until_ready()
 
-@client.event # This event runs whenever a user updates: status, game playing, avatar, nickname or role
-async def on_member_update(before, after): 
-    n = after.nick 
-    if n: # Check if they updated their username
-        if n.lower().count(owner) > 0: # If username contains the owner's username
-            last = before.nick
-            if last: # If they had a username before change it back to that
-                await after.edit(nick=last)
-            else: # Otherwise set it to "LIAR"
-                await after.edit(nick="LIAR")
 
+cogs = ['cogs.utilities']
 #Initiates the bot.
-@client.event
+@bot.event
 async def on_ready():
     await load()
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
-    )
+        )
+    bot.remove_command('help')
+    # Removes the help command
+    # Make sure to do this before loading the cogs
+    for cog in cogs:
+        bot.load_extension(cog)
     await idle()
 
 #Sends a new user a message prompting them to get their role.
-@client.event
+@bot.event
 async def on_member_join(member):
     await load()
     global joined
@@ -84,13 +83,13 @@ async def on_member_join(member):
     await idle()
 
 #Gives user role after reacting to the message in rules.
-@client.event
+@bot.event
 async def on_raw_reaction_add(payload):
     await load()
     message_id = payload.message_id
     #Verifies the message ID
     if message_id == 740026078554357770:
-        guild = discord.utils.find(lambda g : g.name == GUILD, client.guilds)
+        guild = discord.utils.find(lambda g : g.name == GUILD, bot.guilds)
 
         #Looks for the cool_doge emoji.
         if payload.emoji.name == 'cool_doge':
@@ -116,14 +115,14 @@ async def on_raw_reaction_add(payload):
 
 
 #Removes the role if the user removes their emote from the rules message.
-@client.event
+@bot.event
 async def on_raw_reaction_remove(payload):
     await load()
     message_id = payload.message_id
 
     #Verifies the message ID
     if message_id == 740026078554357770:
-        guild = discord.utils.find(lambda g : g.name == GUILD, client.guilds)
+        guild = discord.utils.find(lambda g : g.name == GUILD, bot.guilds)
         
         #Looks for the cool_doge emoji.
         if payload.emoji.name == 'cool_doge':
@@ -136,7 +135,7 @@ async def on_raw_reaction_remove(payload):
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
             if member is not None:
                 await member.remove_roles(role)
-                await client.change_presence(activity=discord.Game('with his foreskin.'))
+                await bot.change_presence(activity=discord.Game('on the floor.'))
                 print(
                     f'{role.name} role taken from {member.name}.')
                 #await client.change_presence(status=discord.Status.dnd, activity=discord.Streaming(name=
@@ -148,27 +147,28 @@ async def on_raw_reaction_remove(payload):
             print(payload.emoji.name)
     await idle()
 
+@bot.event # This event runs whenever a user updates: status, game playing, avatar, nickname or role
+async def on_member_update(before, after): 
+    n = after.nick 
+    if n: # Check if they updated their username
+        if n.lower().count(owner) > 0: # If username contains the owner's username
+            last = before.nick
+            if last: # If they had a username before change it back to that
+                await after.edit(nick=last)
+            else: # Otherwise set it to "LIAR"
+                await after.edit(nick="LIAR")
+
 #Filters certain slurs.
-@client.event 
+@bot.event 
 async def on_message(message):
     await load()
     global messages
     channel = message.channel
     author = message.author
-    mod_channel = client.get_channel(mod_channel_id)
+    mod_channel = bot.get_channel(mod_channel_id)
 
     #This is the start of defining which commands do what.
     if message.content.startswith(cmd_prefix):
-
-#~~~~~~~~~~~~~~~~~~~~~~~~HELP COMMAND~~~~~~~~~~~~~~~~~~~~~~~~~~
-        #Displays the help text.
-        if message.content.startswith('!help'):
-            print(f'{author} sent "!help" command in {channel}.')
-            embed = discord.Embed(title="Bot Commands", description="Explains the different bot commands.")
-            embed.add_field(name="!help", value="Displays this text.")
-            embed.add_field(name="!stats", value="Shows some server statistics.")
-            embed.add_field(name="!mcserver", value="Displays the Minecraft Server ip.")
-            await message.channel.send(content=None, embed=embed)
             
 #~~~~~~~~~~~~~~~~~~~~~~DISCONNECT COMMAND~~~~~~~~~~~~~~~~~~~~~~
         #Disconnects the bot from the websocket.
@@ -180,22 +180,6 @@ async def on_message(message):
             else:
                 await message.delete()
                 print(f"Deleted! Command wasn't in correct channel({bot_cmd_channel}")
-#~~~~~~~~~~~~~~~~~~~~~~~~STATS COMMAND~~~~~~~~~~~~~~~~~~~~~~~~~
-        if message.content.startswith('!stats'):
-            print(f'{author} sent "!stats" command in {channel}.')
-            online = 0
-            total = 0
-            gaming = 0
-            embed = discord.Embed(title="Server Statistics", description="Some statistics about the server.")
-            embed.add_field(name="Online", value=online)
-            embed.add_field(name="Total", value=total)
-            embed.add_field(name="Gaming", value=gaming)
-            await message.channel.send(content=None, embed=embed)
-#~~~~~~~~~~~~~~~~~~~~~~MCSERVER COMMAND~~~~~~~~~~~~~~~~~~~~~~~~
-        if message.content.startswith('!mcserver'):
-            embed = discord.Embed(title= "Minecraft Server IP", description= mcserver_ip)
-            await message.channel.send(content=None, embed=embed)
-            print(f'{author} sent "!mcserver" command in {channel}.')
 #~~~~~~~~~~~~~~~~~~~~~~~~STEAM COMMAND~~~~~~~~~~~~~~~~~~~~~~~~~
         if message.content.startswith('!steam'):
             embed = discord.Embed(title= "Steam Group URL", description= steam_group_url)
@@ -206,16 +190,16 @@ async def on_message(message):
     messages += 1
 #~~~~~~~~~~~~~~~~~~~~~~~~~SLUR FILTER~~~~~~~~~~~~~~~~~~~~~~~~~~
     if channel != mod_channel:
-        for badword in word_filter.words:
+        for badword in b_words:
             if badword in message.content.lower():
                 await message.delete()
                 await message.channel.send(f"{message.author.mention}!\nUh oh!\nYou shouldn't have said that!")
                 if str(author) != bot:
-                    await mod_channel.send(f"@Kings! {message.author} said: '{message.content}'")
+                    await mod_channel.send(f"<@&{mod_role}>! {message.author} said: '{message.content}'")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    await bot.process_commands(message)
     await idle()
 
-
         
-client.loop.create_task(update_stats())
-client.run(TOKEN)
+bot.loop.create_task(update_stats())
+bot.run(TOKEN)
