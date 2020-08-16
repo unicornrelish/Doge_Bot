8ea3058508bfc2e5d007e954c2269b5d07243744
unicornrelish <69436168+unicornrelish@users.noreply.github.com>
2020-08-16 12:28:18 -0400

    Update var.py

--- var.py
+++ var.py
@@ -1,15 +1,16 @@
-#Variables imported to Doge_Bot
-TOKEN = open('token.txt','r').read()
+TOKEN = open('cfn_token.txt','r').read()
 
-ver = '0.0.5'
+ver = '0.1.0'
 owner = 'unicornrelish#7310'
 bot = 'Doge_Bot#5885'
 guild = 'Dogenet Incorporated'
 mod_channel_id = 740025696113655878
 bot_cmd_channel = 'bot_commands'
+mod_role = 739873078791766076
 
 
 steam_group_url = None
-mcserver_ip = 'dogenetmc.duckdns.org'
+mcserver_ip = 'dogenetinc.duckdns.org'
 
-b_words = ['bad words go here']
+b_words = ['nigger', 'kike', 'chink', 'negroid', 'negro', 'injun', 'jigaboo', 'jiggabo', 'jigarooni', 'jijjiboo', 
+'zigabo', 'jigg', 'jigga', 'jigger', 'kyke', 'moon cricket', 'mooncricket', 'niglet', 'nig-nog', 'nignog', 'nigga']
